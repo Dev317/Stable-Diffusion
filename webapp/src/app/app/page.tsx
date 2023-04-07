46442c5caad271da8129c2501573338475cc165b
Bryan Lee <bryanmylee@gmail.com>
2023-04-07 16:19:44 +0800

    feat: move about to /app for state preservation

--- webapp/src/app/app/page.tsx
+++ webapp/src/app/app/page.tsx
@@ -58,7 +58,7 @@ export default function Home({searchParams}: PageParams) {
 
 			<footer className="w-full max-w-5xl mb-32 grid text-center lg:mb-0 lg:grid-cols-3 lg:text-left">
 				<a
-					href="/about"
+					href="/app/about"
 					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 flex flex-col items-center lg:items-start"
 				>
 					<h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
