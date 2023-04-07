037922772082ffaf7a8f5dabab032ae49ec5032f
Bryan Lee <bryanmylee@gmail.com>
2023-04-07 14:19:32 +0800

    feat: show image after submission

--- webapp/src/app/page.tsx
+++ webapp/src/app/page.tsx
@@ -1,9 +1,16 @@
+import Image from "next/image";
 import {Inter} from "next/font/google";
 import FileForm from "./FileForm";
 
 const inter = Inter({subsets: ["latin"]});
 
-export default function Home() {
+interface PageParams {
+	searchParams: {
+		image?: string;
+	};
+}
+
+export default function Home({searchParams}: PageParams) {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -23,7 +30,17 @@ export default function Home() {
 			</div>
 
 			<div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] before:pointer-events-none after:pointer-events-none after:absolute before:-z-10 after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-				<FileForm />
+				{searchParams.image ? (
+					<Image
+						src={searchParams.image}
+						alt="The generated image"
+						width={480}
+						height={480}
+						className="rounded-xl border border-gray-300 dark:border-neutral-800"
+					/>
+				) : (
+					<FileForm />
+				)}
 			</div>
 
 			<footer className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
