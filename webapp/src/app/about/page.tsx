34cbfeff39958373557586709f551c69d860769f
Bryan Lee <bryanmylee@gmail.com>
2023-04-07 12:36:34 +0800

    feat: upload component and about page

--- webapp/src/app/about/page.tsx
+++ webapp/src/app/about/page.tsx
@@ -1,14 +1,13 @@
 import {Inter} from "next/font/google";
-import FileInput from "./FileInput";
 
 const inter = Inter({subsets: ["latin"]});
 
-export default function Home() {
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+export default function About() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
 				<p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-					Audio Thumbnail Generator
+					How we built this
 				</p>
 				<div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
 					<a
@@ -21,18 +20,16 @@ export default function Home() {
 					</a>
 				</div>
 			</div>
-
-			<div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-				<FileInput />
-			</div>
-
+      <p className={`${inter.className} text-3xl tracking-tighter`}>
+        We don&apos;t know lmao.
+      </p>
 			<footer className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
 				<a
-					href="/about"
+					href="/"
 					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
 				>
 					<h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-						Architecture{" "}
+						Back to app{" "}
 						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
 							-&gt;
 						</span>
@@ -40,10 +37,10 @@ export default function Home() {
 					<p
 						className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
 					>
-						Learn more about how we built this.
+						Create more audio thumbnails.
 					</p>
 				</a>
 			</footer>
-		</main>
-	);
+    </main>
+  )
 }
--- webapp/tailwind.config.js
+++ webapp/tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
 	],
 	theme: {
 		extend: {
+			colors: {
+				foreground: 'rgb(var(--foreground-rgb) / <alpha-value>)',
+			},
 			backgroundImage: {
 				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
 				"gradient-conic":
