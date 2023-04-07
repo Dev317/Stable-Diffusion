5c6f2da2d3c8469febbd0b4df1649fae87a197e5
Bryan Lee <bryanmylee@gmail.com>
2023-04-07 14:43:42 +0800

    style: nit

--- webapp/src/app/FileUpload.tsx
+++ webapp/src/app/FileUpload.tsx
@@ -1,4 +1,5 @@
 "use client";
+import {Inter} from "next/font/google";
 import {Dispatch, useCallback} from "react";
 import {FilePondFile} from "filepond";
 import {FilePond, registerPlugin} from "react-filepond";
@@ -7,6 +8,8 @@ import FilePondPluginMediaPreview from "filepond-plugin-media-preview";
 import "filepond/dist/filepond.min.css";
 import "filepond-plugin-media-preview/dist/filepond-plugin-media-preview.min.css";
 import "./FileUpload.css";
+
+const inter = Inter({subsets: ["latin"]});
 
 registerPlugin(FilePondPluginMediaPreview);
 
@@ -29,7 +32,7 @@ export default function FileUpload({
 	);
 
 	return (
-		<div className="w-96 min-h-20">
+		<div className={`${inter.className} w-96 min-h-20`}>
 			<FilePond
 				name="wav_file"
 				files={files}
--- webapp/src/app/page.tsx
+++ webapp/src/app/page.tsx
@@ -56,7 +56,7 @@ export default function Home({searchParams}: PageParams) {
 				)}
 			</div>
 
-			<footer className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+			<footer className="w-full mb-32 grid text-center lg:mb-0 lg:grid-cols-3 lg:text-left">
 				<a
 					href="/about"
 					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
